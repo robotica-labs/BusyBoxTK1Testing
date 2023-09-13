1757f5a10965d867e3171aecd2e0beaeaddb87bb
Matthew Lawrence <matthew.lawrence@robotica-labs.com>
2023-09-12 22:12:09 -0500

    Updates

--- config.sh
+++ config.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 echo "$(pwd)"
 cd ./busybox-1.36.1/
-read -p "Press enter to continue"
-read -p "stop"
 make menuconfig
 make
 make install CONFIG_PREFIX=../Linux_for_Tegra/rootfs/
 cd ../Linux_for_Tegra/rootfs/
-sudo mkdir dev etc lib proc tmp sys
-sudo cp -dpR TK1_Sample_File_System/dev/* ../Linux_for_Tegra/rootfs/dev/
+mkdir dev etc lib proc tmp sys
+cp -dpR  TK1_Sample_File_System/dev/ Linux_for_Tegra/rootfs/dev/
+
+Linux_for_Tegra/rootfs/dev/
+TK
+"proc  /proc proc  defaults  0 0"
+"none  /tmp  ramfs defaults  0 0"
+"mdev  /dev  ramfs defaults  0 0"
+"sysfs /sys  sysfs defaults  0 0"
+
 #Optional device setup would go here
